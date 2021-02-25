1900b3e3642bbed4b0df3db12bdb06f6ce3e51ad
grintann <grintann2020@gmail.com>
2021-02-26 00:42:45 +0800

    update minSdk to 23

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(FxApp());
+
+class FxApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('TTT'),
+        ),
+      ),
+    );
+  }
+}
